cd893aec1b6793c22558de1caa2817357b804dbb
はむ <hawks_2023@outlook.jp>
2024-09-03 01:02:59 +0900

    OnPlayerCollisionEnterおかしくね？

---
+++ Assets/Scripts/BallUdon.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class BallUdon : UdonSharpBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    public override void OnPlayerCollisionEnter(VRCPlayerApi player)
+    {
+
+        /*  VRCPlayerApi player = collision.gameObject.GetComponent<VRCPlayerApi>(); */
+        if (player != null && player == Networking.LocalPlayer)
+        {
+            Debug.Log("hit");
+
+        }
+
+    }
+
+}
