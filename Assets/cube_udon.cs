14054d29828123f07e3645de2cac01c0f97808ca
--global <--global>
2024-09-03 00:46:48 +0900

    Use 可能なcubeの追加

---
+++ Assets/cube_udon.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common;
+
+public class cube_udon : UdonSharpBehaviour
+{
+    bool cubeIsGold = true;
+    public Material[] cubeMaterials = new Material[2];
+    public override void InputDrop(bool value, UdonInputEventArgs args)
+    {
+        if(value){
+            base.InputDrop(value, args);
+            if (cubeIsGold)
+            {
+                GetComponent<MeshRenderer>().material = cubeMaterials[1];
+                cubeIsGold = false;
+            }
+            else
+            {
+                GetComponent<MeshRenderer>().material = cubeMaterials[0];
+                cubeIsGold = true;
+            }
+        }
+    }
+}
