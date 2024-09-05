c23bad27bcfab51da8cb89028e93b4530b0adcc2
はむ <hawks_2023@outlook.jp>
2024-09-06 01:30:09 +0900

    サッカーボールの動きできた！

--- Assets/Scripts/BallUdon.cs
+++ Assets/Scripts/BallUdon.cs
@@ -11,20 +11,32 @@ public class BallUdon : UdonSharpBehaviour
     public Env Env;
     void Start()
     {
+        Debug.Log("co");
+        Hukkatu();
+    }
+    void Hukkatu()
+    {
+        Debug.Log("lli");
+        Collider collider = GetComponent<Collider>();
+        if (collider != null)
+        {
 
+            collider.enabled = true;
+
+        }
     }
 
     public override void OnPlayerTriggerEnter(VRCPlayerApi player)
     {
 
-        /*  VRCPlayerApi player = collision.gameObject.GetComponent<VRCPlayerApi>(); */
-        if (player != null && player == Networking.LocalPlayer)
+
+        /* if (player != null && player == Networking.LocalPlayer)
         {
             Debug.Log("hit");
             Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
             rigidbody.AddForce(1, 0, 0);
 
-        }
+        } */
 
     }
 
--- Assets/Scripts/PlayerTower.cs
+++ Assets/Scripts/PlayerTower.cs
@@ -14,14 +14,23 @@ public class PlayerTower : UdonSharpBehaviour
     {
 
         this.transform.position = Networking.LocalPlayer.GetPosition();
+        this.transform.rotation = Networking.LocalPlayer.GetRotation();
     }
     public void OnTriggerEnter(Collider other)
     {
 
         objName = other.gameObject.name;
-        if (objName == Env.FieldBall)
+        if (objName == Env.BBall)
         {
             Debug.Log(objName);
+            Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+            /*  rigidbody.AddForce(1000, 100, 0); */
+            rigidbody.angularVelocity = Vector3.zero;
+            rigidbody.AddForce(transform.forward * Env.KickForce, ForceMode.Impulse);
+            rigidbody.AddForce(transform.up * Env.KickForceUp, ForceMode.Impulse);
+            rigidbody.AddTorque(transform.right * Env.KickForceRotate, ForceMode.Impulse);
+
+
         }
 
         /* if (this.gameObject.Tag == "Ball")
