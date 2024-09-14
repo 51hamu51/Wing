5ef428d5c05287363e8e9171d21c61f681ef5bc3
はむ <hawks_2023@outlook.jp>
2024-09-14 17:00:00 +0900

    ミラー変更、スイッチのインポート

---
+++ Assets/MirrorHQLQ/Switch_Mirror.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Switch_Mirror : UdonSharpBehaviour
+{
+
+    public GameObject targetMirror;
+    public GameObject targetMirrorSwitchOn;
+    public GameObject targetMirrorSwitchOff;
+    public GameObject oppositMirror;
+    public GameObject oppositMirrorSwitchOn;
+    public GameObject oppositMirrorSwitchOff;
+
+    
+    public override void Interact()
+    {
+        targetMirror.SetActive(!targetMirror.activeSelf);
+        targetMirrorSwitchOn.SetActive(!targetMirrorSwitchOn.activeSelf);
+        targetMirrorSwitchOff.SetActive(!targetMirrorSwitchOff.activeSelf);
+        
+        oppositMirror.SetActive(false);
+        oppositMirrorSwitchOn.SetActive(false);
+        oppositMirrorSwitchOff.SetActive(true);
+    }
+}
