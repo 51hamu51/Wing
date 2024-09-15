767faa21be3f2d26eeb0aa5efb93326964dadc18
はむ <hawks_2023@outlook.jp>
2024-09-15 18:15:44 +0900

    tokei

---
+++ Assets/MIKI_DigiC_SDK3/script/MikiClockTimeOnly.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MikiClockTimeOnly : UdonSharpBehaviour
+{
+    #region プライベート定義群
+    /// <summary>
+    /// スクリプト対象のテキスト
+    /// </summary>
+    [SerializeField] private Text targetText;
+    /// <summary>
+    /// 現在時間
+    /// </summary>
+    private DateTime now;
+    #endregion
+
+    void Start()
+    {
+        now = DateTime.Now;
+
+        targetText.text = now.ToString("HH:mm:ss");
+    }
+
+    void Update()
+    {
+        now = DateTime.Now;
+        if (targetText.text != now.ToString("HH:mm:ss")) targetText.text = now.ToString("HH:mm:ss");
+    }
+
+}
