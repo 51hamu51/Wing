3f8d8f50f18eddeff6d243493514a1e0037ba500
はむ <hawks_2023@outlook.jp>
2024-09-17 01:13:19 +0900

    これで無理ならあきらめる

---
+++ Assets/ax-item/Quiz/Script/Quiz_Btn.cs
@@ -0,0 +1,33 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
+
+public class Quiz_Btn : UdonSharpBehaviour
+{
+
+    public string text;
+    public int num;
+    public UdonBehaviour motherboard;
+
+    void Start()
+    {
+    }
+
+    public void trigger()
+    {
+        int[] ans = (int[])motherboard.GetProgramVariable("current_select");
+        for (int i = 0; i < ans.Length; i++)
+        {
+            if (ans[i] == num)
+            {
+                motherboard.SendCustomEvent("correct");
+                return;
+            }
+        }
+        motherboard.SendCustomEvent("incorrect");
+
+    }
+}
