0b3f06e382be330faef2ff75f9129964cfd2914f
はむ <hawks_2023@outlook.jp>
2024-09-19 12:49:51 +0900

    select

---
+++ Assets/Scripts/AnkSelect.cs
@@ -0,0 +1,21 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class AnkSelect : UdonSharpBehaviour
+{
+    public Env Env;
+
+    void Start()
+    {
+
+        Env.AnkSelectText.text = string.Format("{0} / {1} / {2}", Env.AnkPre[Env.Ank[0]], Env.AnkGene[Env.Ank[1]], Env.AnkFeed[Env.Ank[2]]);
+
+    }
+    void Update()
+    {
+        Env.AnkSelectText.text = string.Format("{0} / {1} / {2}", Env.AnkPre[Env.Ank[0]], Env.AnkGene[Env.Ank[1]], Env.AnkFeed[Env.Ank[2]]);
+    }
+}
